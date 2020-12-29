3190519d0231dd3b45a0128cdc4c4210cbfcc75b
AkshayaAcharya <acharya.akshaya04@gmail.com>
2020-12-29 20:31:02 +0530

    Migrate to Github

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -1,135 +1,151 @@
-/////////////////////////////////////////////////////////////////////////////
 
-/////////////////////// Do not modify the below code ////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////
+// Don't touch the below code
 
 (function() {
-    function buildQuiz() {
-        // we'll need a place to store the HTML output
-        const output = [];
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
 
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-            // we'll want to store the list of answer choices
-            const answers = [];
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
 
-            // and for each available answer...
-            for (letter in currentQuestion.answers) {
-                // ...add an HTML radio button
-                answers.push(
-                    `<label>
-          <input type="radio" name="question${questionNumber}" value="${letter}">
-          ${letter} :
-          ${currentQuestion.answers[letter]}
-        </label>`
-                );
-            }
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
 
-            // add this question and its answers to the output
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-      <div class="answers"> ${answers.join("")} </div>`
-            );
-        });
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
 
-        // finally combine our output list into one string of HTML and put it on the page
-        quizContainer.innerHTML = output.join("");
-    }
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
 
-    function showResults() {
-        // gather answer containers from our quiz
-        const answerContainers = quizContainer.querySelectorAll(".answers");
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-        // keep track of user's answers
-        let numCorrect = 0;
+    // keep track of user's answers
+    let numCorrect = 0;
 
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-            // find selected answer
-            const answerContainer = answerContainers[questionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-            // if answer is correct
-            if (userAnswer === currentQuestion.correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
 
-                // color the answers green
-                //answerContainers[questionNumber].style.color = "lightgreen";
-            } else {
-                // if answer is wrong or blank
-                // color the answers red
-                answerContainers[questionNumber].style.color = "red";
-            }
-        });
+        // color the answers green
+        //answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
 
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
 
-    const quizContainer = document.getElementById("quiz");
-    const resultsContainer = document.getElementById("results");
-    const submitButton = document.getElementById("submit");
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
 
 
-    /////////////////////////////////////////////////////////////////////////////
-
-    /////////////////////// Do not modify the above code ////////////////////////
-
-    /////////////////////////////////////////////////////////////////////////////
+// Don't touch the above code
 
 
 
 
+// Write your MCQs here --- Start --- --------------------
 
-
-    /////////////// Write the MCQ below in the exactly same described format ///////////////
-
-
-    const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
-            answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
-            },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
-        },
-
+  const myQuestions = [
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: " Which of the following statement is correct?<br>1. For grade A aggregate 10 abrasive charges are used<br>2. For grade B aggregate 12 abrasive charges are used<br>3. For grade A aggregate 12 abrasive charges are used<br>4. For grade B aggregate 10 abrasive charges are used ",
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
+        a: "1 is correct",
+        b: "2 and 3 is correct",
+        c: "3 and 4 is correct",
+        d: "None of the above"
+      },
+      correctAnswer: "b"
     },
 
+    {
+      question: "Los Angeles Abrasion test gives information about which property of aggregate?",
+      answers: {
+        a: "Compressive strength",
+        b: "Hardness",
+        c: "Crushing",
+        d: "Impact strength"
+      },
+      correctAnswer: "b"
+    },
 
-        
-    ];
+    {
+      question: "Machine rotates at the speed of?",
+      answers: {
+        a: "20 to 30 revolution /min",
+        b: "30 to 33 revolution /min",
+        c: "20 to 35 revolution /min",
+        d: "20 to 37 revolution /min"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "If W<sub>1</sub>= Total weight of sample, W<sub>2</sub>= final weight of sample, W<sub>3</sub>= Weight of sample passing in 1.7mm sieve, Then percentage wear is",
+      answers: {
+        a: "((W<sub>3</sub>-W<sub>1</sub>) /W<sub>2</sub>) * 100",
+        b: "((W<sub>1</sub>-W<sub>2</sub>)/W<sub>2</sub>) * 100",
+        c: "((W<sub>1</sub>-W<sub>2</sub>)/W<sub>1</sub>) * 100",
+        d: "(W<sub>1</sub>/W<sub>2</sub>) * 100"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Slag and limestone have low Los Angeles abrasion loss. (say True or False)",
+      answers: {
+        a: "True",
+        b: "False"
+      },
+      correctAnswer: "b"
+    }
+  ];
 
 
 
 
-    /////////////////////////////////////////////////////////////////////////////
-
-    /////////////////////// Do not modify the below code ////////////////////////
-
-    /////////////////////////////////////////////////////////////////////////////
+// ---------------------------- End -------------------------------
 
 
-    // display quiz right away
-    buildQuiz();
 
-    // on submit, show results
-    submitButton.addEventListener("click", showResults);
+
+
+
+
+
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
 })();
-
-
-/////////////////////////////////////////////////////////////////////////////
-
-/////////////////////// Do not modify the above code ////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////
--- experiment/theory.md
+++ experiment/theory.md
@@ -1,1 +1,57 @@
-### Link your theory in here
+### INTRODUCTION<br>
+
+<p style="text-indent:50px; text-align:justify;">Aggregates undergo significant wear and tear throughout their life. Aggregates must be hard and tough enough to resist crushing, degradation and disintegration and be able to transmit loads from the pavement surface to the underlying layers and eventually the subgrade. Testing the strength of parent rock alone does not exactly indicate the strength of aggregates in concrete. For this reason assessment of strength of the aggregates are made by using a sample bulk aggregates in standardized manner. The principal mechanical property of aggregate required in any construction is</p>
+
+<ol style="text-indent:25px; text-align:justify;list-style-position: inside">
+<li>Satisfactory resistance to crushing under the roller during construction</li>
+<li>Adequate resistance to surface abrasion under traffic</li>
+</ol>
+
+<p style="text-indent:50px; text-align:justify;">Aggregates used in road construction should be strong enough to resist abrasion and crushing under traffic wheel load. If aggregates are weak the stability of pavement structure is adversely affected. If the aggregates used are not resistant to abrasion it may cause premature failure or a loss of skid resistance of pavements, also poor resistance to abrasion can produce excessive dust. Abrasion test is carried out on the aggregate sample to test the hardness property of aggregates and to decide whether they are suitable for different pavement construction works. Los Angeles test is widely used to test abrasion of aggregate and the method has been standardized in India (IS:2386 part-IV). The Los Angeles abrasion test finds the percentage wear due to relative rubbing action between the aggregate and steel balls.</p>
+
+<p style="text-indent:50px; text-align:justify;">Los Angeles machine has a circular rotating drum of length 520 mm and internal diameter 700 mm mounted on horizontal axis. An abrasive charge consisting of cast iron spherical balls of 48 mm diameters and weight 340-445 g is placed in the cylinder along with the aggregates based on the selected grading. The number of the abrasive spheres varies according to the grading of the sample. The quantity of aggregates to be used depends upon the grading and usually ranges from 5-10 kg. The cylinder is then locked and rotated at the speed of 30-33 rpm for a total of 500 -1000 revolutions depending upon the gradation of aggregates. After specified revolutions, the material passing 1.7 mm IS sieve is measured and expressed as percentage total weight of the sample. This value is called Los Angeles abrasion value.</p>
+
+<p style="text-indent:50px; text-align:justify;">The table below shows limits of Los Angeles abrasion value for different types of road construction:</p>
+<center>
+<table style="width:800px;margin-left: 0;text-align:center;">
+<tr style="text-align:center">
+<th style="text-align:center;height:50px;width:2%">Type of Pavement</th>
+<th style="text-align:center;height:50px;width:0.5%">Max. permissible abrasion value in %</th>
+</tr>
+<tr style="text-align:center;height:50px;">
+<td> Water bound macadam sub base course</td>
+<td> 60</td>
+</tr>
+<tr style="text-align:center;height:50px;">
+<td>WBM base course with bituminous surfacing</td>
+<td>50</td>
+</tr>
+<tr style="text-align:center;height:50px;">
+<td>Bituminous bound macadam</td>
+<td>50</td>
+</tr>
+<tr style="text-align:center;height:50px;">
+<td>WBM surfacing course</td>
+<td>40</td>
+</tr>
+ <tr style="text-align:center;height:50px;">
+<td>Bituminous penetration macadam</td>
+<td>40</td>
+</tr>
+<tr style="text-align:center;height:50px;">
+<td>Bituminous surface dressing, cement concrete surface course</td>
+<td>35</td>
+</tr>
+<tr style="text-align:center;height:50px;">
+<td>Bituminous concrete surface course</td>
+<td>30</td>
+</tr>
+</table></center>
+
+<center>(Source: The Constructor, Civil Engineering Home)</center><br>
+
+<p style="text-indent:50px; text-align:justify;"><strong>Relevant Indian Standard for Los Angeles Abrasion Test on Aggregate</strong>:</p>
+<ol style="text-indent:25px; text-align:justify;list-style-position: inside">
+<li>IS 2386 (Part IV) 1963: Methods of Test for Aggregates Mechanical Properties, Tenth Reprint MARCH 1997.</li>
+<li>IS 383-1970: Specification for Coarse and Fine Aggregates.</li>
+</ol>
